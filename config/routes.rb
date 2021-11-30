a9cbd6e61911b86921c9178261caf55d7dfedf14
thomas le veve <thomas.l.v@hotmail.fr>
2021-11-30 12:24:52 +0100

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :rooms, only: [:index, :show]
+
+  namespace :host do
+    root to: 'pages#home'
+
+    resources :housing_requests, only: [:index] do
+      member do
+        patch :decline
+        patch :accept
+      end
+    end
+    resources :rooms, only: [:new, :create, :edit, :update]
+  end
+
+  namespace :seeker do
+    resources :rooms, only: [] do
+      resources :housing_requests, only: [:create]
+    end
+    resource :profile, only: [:edit, :update]
+  end
 end
