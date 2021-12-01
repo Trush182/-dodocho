f6f12f39f2e0cffbe1646f76f4fe6101e4f8a51c
clemence ogeron <ogeron.clemence@orange.fr>
2021-12-01 17:44:03 +0100

    back-end de room#index

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Cleaning DB..."
+HousingRequest.destroy_all
 Room.destroy_all
 User.destroy_all
-HousingRequest.destroy_all
 
 # SEEKERS **************************************************************
 
