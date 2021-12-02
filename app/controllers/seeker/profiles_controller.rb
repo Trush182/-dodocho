c4ac6d4a1350fef5eca8e66166738d6e0e17a556
clemence ogeron <ogeron.clemence@orange.fr>
2021-12-02 11:20:19 +0100

    modif seed

--- app/controllers/seeker/profiles_controller.rb
+++ app/controllers/seeker/profiles_controller.rb
@@ -5,7 +5,7 @@ class Seekers::ProfilesController < ApplicationController
   end
 
   def update
-    @profile.update(summary: params[:room][:summary], address: params[:room][:address], housing_types: params[:room][:housing_types])
+    @profile.update(summary: params[:user][:summary], address: params[:room][:address], housing_types: params[:room][:housing_types])
 
     redirect_to room_path
   end
