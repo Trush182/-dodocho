b3c9ed5174882ad4979f1b0190469e8a8c39480c
thomas le veve <thomas.l.v@hotmail.fr>
2021-12-03 16:20:34 +0100

    Setup home + offered services with minimal front

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def service_name_to_skill(service)
+    {
+      "Jardinage" => "gardening",
+      "Courses" => "shopping",
+      "Bricolage" => "crafting",
+      "Cuisine" => "cooking",
+      "Animaux" => "pet-walking"
+    }[service]
+  end
 end
