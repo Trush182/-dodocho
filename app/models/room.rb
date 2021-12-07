dda0df8040dd4d0d1a566ce8f037769f0b80120a
clemence ogeron <ogeron.clemence@orange.fr>
2021-12-07 09:30:16 +0100

    ajout button caroussel

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,6 @@
 class Room < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  
   has_many_attached :photos
 
   belongs_to :host, class_name: "User"
