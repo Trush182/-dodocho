2b0a172d34e6553a5f9a64bb30985a31f5affe21
Firas Oueslati <firas.oueslati@audencia.com>
2021-12-09 12:53:04 +0100

    new files of algolia

--- app/controllers/host/housing_requests_controller.rb
+++ app/controllers/host/housing_requests_controller.rb
@@ -15,44 +15,35 @@ class Host::HousingRequestsController < ApplicationController
     @housing_request.status = "accepted"
     @housing_request.save
 
-
     Client.messages.create(
-from: '+12312448223',
-to: '+33661634782',
-body: "Hey friend, it is accepted"
- )
-
-         redirect_to host_housing_requests_path
-
+                         from: '+12312448223',
+                          to: '+33661634782',
+                          body: "Hey friend, it is accepted"
+                        )
+    redirect_to host_housing_requests_path
   end
 
 
 #     TwilioClient.messages.create(
-
 #                   body: ".. accepted your request. His/her number is",
 #                   # {current_user.name}
 #                   # {current_user.phone_number}. It's your turn now!"
 #                   from: '+12312448223',
 #                   # to: "#{contact_request.seeker.phone_number}"
-#                   to: "+33661634782"
-#                   )
+#                   to: "+33661634782")
 
   def decline
     @housing_request = HousingRequest.find(params[:id])
     @housing_request.status = "declined"
     @housing_request.save
 
-
-
     Client.messages.create(
-from: '+12312448223',
-to: '+33661634782',
-body: "Hey friend, it is declined"
- )
-
+                         from: '+12312448223',
+                          to: '+33661634782',
+                          body: "Hey friend, it is declined"
+                        )
     redirect_to host_housing_requests_path
   end
-
 
     # TwilioClient.messages
     #             .create(
--- app/models/housing_request.rb
+++ app/models/housing_request.rb
@@ -1,13 +1,10 @@
 class HousingRequest < ApplicationRecord
+  Client = Twilio::REST::Client.new('AC7985a1d987860c452b1b15da7440c284', '7f1c0d4bbf71513a512febee6685f710')
   MAX_NIGHTS_COUNT = 3
   HOUR_BEFORE_POSTPONING_REQUESTS_TO_NEXT_DAY = 23
-  Client = Twilio::REST::Client.new('AC7985a1d987860c452b1b15da7440c284', '7f1c0d4bbf71513a512febee6685f710')
-
-  require 'twilio-ruby'
 
   belongs_to :room
   belongs_to :seeker, class_name: "User"
-  belongs_to :host, class_name: "User"
 
   validates :start_date, presence: true
   validates :end_date, presence: true
@@ -19,14 +16,14 @@ class HousingRequest < ApplicationRecord
     self.status = :pending
   end
 
+
   private
 
   def send_message
-    Client.messages.create(
-      from: '+12312448223',
-      to: '+33661634782',
-      body: "Thomas voudrait bien être hébergé chez toi! Go to http://www.dodocho.top/"
-    )
+        Client.messages.create(
+                         from: '+12312448223',
+                          to: '+33661634782',
+                          body: "Hey friend, you have a new request"
+                        )
   end
-
 end
