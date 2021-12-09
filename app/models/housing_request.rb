fed1cd8d9a32f3596470e2f9f296664e69741e57
Firas Oueslati <firas.oueslati@audencia.com>
2021-12-09 15:29:26 +0100

    twilio fixing by firas

--- app/models/housing_request.rb
+++ app/models/housing_request.rb
@@ -1,5 +1,5 @@
 class HousingRequest < ApplicationRecord
-  Client = Twilio::REST::Client.new('AC7985a1d987860c452b1b15da7440c284', '7f1c0d4bbf71513a512febee6685f710')
+  Client = Twilio::REST::Client.new('AC7985a1d987860c452b1b15da7440c284', '81b5f2213cbae5f346a74c7a4351576b')
   MAX_NIGHTS_COUNT = 3
   HOUR_BEFORE_POSTPONING_REQUESTS_TO_NEXT_DAY = 23
 
@@ -24,7 +24,7 @@ class HousingRequest < ApplicationRecord
         Client.messages.create(
                          from: '+12312448223',
                           to: '+33661634782',
-                          body: "Hey friend, you have a new request"
+                          body: 'Hey friend, you have a new request'
                         )
   end
 end
