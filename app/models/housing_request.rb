10419778e595e85965925ef010a476f026ca5e5d
Firas Oueslati <firas.oueslati@audencia.com>
2021-12-09 12:58:26 +0100

    algolia implement try

--- app/models/housing_request.rb
+++ app/models/housing_request.rb
@@ -6,6 +6,7 @@ class HousingRequest < ApplicationRecord
   belongs_to :room
   belongs_to :seeker, class_name: "User"
 
+
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :status, presence: true
