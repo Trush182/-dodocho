e76c4a68af0329dd7197ffaa3327383f594946c3
Firas Oueslati <firas.oueslati@audencia.com>
2021-12-09 11:53:02 +0100

    SMS testing implementation in model and controller

--- app/controllers/host/housing_requests_controller.rb
+++ app/controllers/host/housing_requests_controller.rb
@@ -1,4 +1,7 @@
+
 class Host::HousingRequestsController < ApplicationController
+  TwilioClient = Twilio::REST::Client.new(ENV['AC7985a1d987860c452b1b15da7440c284'], ENV['7f1c0d4bbf71513a512febee6685f710'])
+
   def index
     @requests = current_user.housing_requests.includes(:room).order(created_at: :desc)
   end
@@ -8,6 +11,15 @@ class Host::HousingRequestsController < ApplicationController
     @housing_request.status = "accepted"
     @housing_request.save!
 
+    TwilioClient.messages.create(
+                  body: ".. accepted your request. His/her number is",
+                  # {current_user.name}
+                  # {current_user.phone_number}. It's your turn now!"
+                  from: '+12312448223',
+                  # to: "#{contact_request.seeker.phone_number}"
+                  to: "+33661634782"
+                  )
+
     redirect_to host_housing_requests_path
   end
 
@@ -15,7 +27,20 @@ class Host::HousingRequestsController < ApplicationController
     @housing_request = HousingRequest.find(params[:id])
     @housing_request.status = "declined"
     @housing_request.save!
+    TwilioClient.messages
+                .create(
+                  body: " declined your request... :(",
+                  #{current_user.name}
+                  from: '+12312448223',
+                  # to: "#{contact_request.explorer.phone_number}"
+                  to: '+33661634782'
+                  )
 
     redirect_to host_housing_requests_path
   end
+
+  # def show
+  #   @housing_request = HousingRequest.find(params[:id])
+  # end
+
 end
--- app/models/housing_request.rb
+++ app/models/housing_request.rb
@@ -1,15 +1,35 @@
 class HousingRequest < ApplicationRecord
   MAX_NIGHTS_COUNT = 3
   HOUR_BEFORE_POSTPONING_REQUESTS_TO_NEXT_DAY = 23
+  TwilioClient = Twilio::REST::Client.new(ENV['AC7985a1d987860c452b1b15da7440c284'], ENV['7f1c0d4bbf71513a512febee6685f710'])
+
 
   belongs_to :room
   belongs_to :seeker, class_name: "User"
+  belongs_to :host, class_name: "User"
 
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :status, presence: true
 
+  after_create :send_message
+
   def pending!
     self.status = :pending
   end
+
+  private
+
+  def send_message
+    TwilioClient.messages.create(
+                  # {self.seeker.name}
+                  from: '+12312448223',
+                  #  to: "#{self.guide.phone_number}"
+                  to: "+33661634782",
+                  body: "Thomas voudrait bien être hébergé chez toi! Go to http://www.dodocho.top/"
+                  )
+  end
+
 end
+
+
